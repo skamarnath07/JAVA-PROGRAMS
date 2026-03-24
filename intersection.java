abd0f742592ae7f47c4d11eeab36578a037acd0d
SK AMARNATH <skamarnath07@gmail.com>
2026-03-24 23:28:26 +0530

    Add files via upload

---
+++ intersection.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+public class intersection {
+
+    public static void main(String[] args) {
+        int[] a={1,2,2,3,3,4,5,6};
+        int[] b={2,3,3,5,6,6,7};
+        List<Integer> ans=new ArrayList<>();
+        int n1=a.length;
+        int n2=b.length;
+        int i=0;
+        int j=0;
+        while( i<n1 && j<n2){
+            if(a[i]==b[j])
+            {
+                ans.add(a[i]);
+                i++;
+                j++;
+            }
+            else{
+                    i++;
+                
+            }
+        }
+        System.out.println(ans.toString());
+        
+    }
+}
